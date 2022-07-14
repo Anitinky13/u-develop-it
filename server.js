a83d1cf7372a72b92572777eeaa264d3f9a8fbe7
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-07-14 13:00:48 -0500

    connected the database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,21 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //your my sql username,
+    user: "root",
+    //your mysql password
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // app.get("/", (req, res) => {
 //   res.json({
 //     message: "Hello World",
